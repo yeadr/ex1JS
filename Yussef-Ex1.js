b91d3837b7f93a7bfeb5ef97bce807da58557b0d
yeadr <yussef.elalami@institutvidreres.cat>
2025-11-06 09:35:13 +0100

    Ex 2

--- Yussef-Ex1.js
+++ Yussef-Ex1.js
@@ -2,3 +2,15 @@ console.log("Exercici 1:")
 console.log("El meu nom és Yussef")
 console.log("El meu cognom és El Alami")
 console.log("He nascut al 2008")
+
+console.log("")
+console.log("Exercici 2")
+let nom = "Juan"
+let edat = 25
+let estudiant = true
+console.log(`${nom} té ${edat} anys, es estudiant?`)
+if (estudiant) {
+    console.log("Si")
+} else {
+    console.log("No")
+}
